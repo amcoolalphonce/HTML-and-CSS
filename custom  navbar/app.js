94ef0352495ecf1e23d635485e059f142add6463
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-11-27 22:18:16 +0300

    Update app.js

--- custom  navbar/app.js
+++ custom  navbar/app.js
@@ -2,5 +2,6 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
 menu.addEventListener('click', function ()
 {
-        
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
 })
